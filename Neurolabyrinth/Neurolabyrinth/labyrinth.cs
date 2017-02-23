bc7a1980a70c0c101f396951fceb1b7e4b0676ab
Awesome327 <russe-stefan@hotmail.de>
2017-02-23 14:15:12 +0100

    Labyrinth class

---
+++ Neurolabyrinth/Neurolabyrinth/labyrinth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neurolabyrinth
+{
+    class labyrinth
+    {
+        //Größe und Breite des Labyrinths
+        const int row = 10;
+        const int column = 10;
+
+        //Labyrinth array
+        private int[,] laby = new int[row, column];
+
+        //Konstruktor
+        public labyrinth() { }
+
+        //Getter Setter Labyrinth
+        public int[,] Laby
+        {
+            get { return laby; }
+            set { laby = value; }
+        }
+
+
+    }
+}
