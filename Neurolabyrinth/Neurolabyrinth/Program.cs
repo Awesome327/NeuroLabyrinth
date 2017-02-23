e9f8fc9d006acbca49638ca571ba8cf1fb5af01b
Awesome327 <russe-stefan@hotmail.de>
2017-02-23 13:58:38 +0100

    Layers & connectors

--- Neurolabyrinth/Neurolabyrinth/Program.cs
+++ Neurolabyrinth/Neurolabyrinth/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NeuronDotNet.Core.Backpropagation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,18 @@ namespace Neurolabyrinth
 {
     class Program
     {
+        public static BackpropagationNetwork network;
+
         static void Main(string[] args)
         {
+            //Layer
+            LinearLayer inputLayer = new LinearLayer(2);
+            SigmoidLayer hiddenLayer = new SigmoidLayer(2);
+            SigmoidLayer outputLayer = new SigmoidLayer(1);
+
+            //Connectors
+            BackpropagationConnector connector = new BackpropagationConnector(inputLayer, hiddenLayer);
+            BackpropagationConnector connector2 = new BackpropagationConnector(hiddenLayer, outputLayer);
         }
     }
 }
